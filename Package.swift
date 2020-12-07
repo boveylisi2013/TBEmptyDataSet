e4e816d4ccc376494935898c69a179d9496f7916
Si Li <boveylisi@gmail.com>
2020-12-07 12:32:16 -0800

    refactor folder structure for swift package

--- Package.swift
+++ Package.swift
@@ -3,10 +3,7 @@ import PackageDescription
 
 let package = Package(name: "TBEmptyDataSet",
   platforms: [
-    .macOS(.v10_12),
-    .iOS(.v10),
-    .tvOS(.v10),
-    .watchOS(.v3)
+    .iOS(.v11)
   ],
   products: [
     .library(name: "TBEmptyDataSet", targets: ["TBEmptyDataSet"])
